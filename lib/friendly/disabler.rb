ce3d768a181b027c56b6ffd487a6f397578de56b
Andre Linoge <andrelinoge87@gmail.com>
2019-04-17 08:14:59 -0700

    wip

--- lib/friendly/disabler.rb
+++ lib/friendly/disabler.rb
@@ -3,24 +3,30 @@ require "friendly/disabler/core_ext"
 
 module Friendly
   module Disabler
-    THREAD_LOCAL_KEY = :__friendly_disabler_state
+    FD_THREAD_KEY = :__friendly_disabler_state
 
     class << self
 
-      # Return true if friendly_id is currently disabled
-      def friendly_id_disabled?
-        !!Thread.current[THREAD_LOCAL_KEY]
+      def friendly_id_disable!
+        Thread.current[FD_THREAD_KEY] = true
       end
 
-      # Run a block with friendly id disabled
+      def friendly_id_enable!
+        Thread.current[FD_THREAD_KEY] = false
+      end
+
+      def friendly_id_disabled?
+        !!Thread.current[FD_THREAD_KEY]
+      end
+
       def disable_friendly_id(&block)
         begin
-          old_value = Thread.current[THREAD_LOCAL_KEY]
-          Thread.current[THREAD_LOCAL_KEY] = true
+          old_value = Thread.current[FD_THREAD_KEY]
+          Thread.current[FD_THREAD_KEY] = true
 
           block.call
         ensure
-          Thread.current[THREAD_LOCAL_KEY] = old_value
+          Thread.current[FD_THREAD_KEY] = old_value
         end
       end
     end
---
+++ test/dummy/app/models/dummy_blog.rb
@@ -0,0 +1,2 @@
+class DummyBlog < ApplicationRecord
+end
