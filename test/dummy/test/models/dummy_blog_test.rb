1fc81a2ddc494072f303a8410eb2a99d65626d7b
Andre Linoge <andrelinoge87@gmail.com>
2019-04-26 05:44:20 -0700

    wip

--- test/dummy/test/models/dummy_blog_test.rb
+++ test/dummy/test/models/dummy_blog_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class DummyBlogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    binding.pry
+    assert true
+
+  end
 end
